3b9b893dc609d8ee63579774f85aff16d15a7620
Mountaga Diao Leye Diop <mdl.diop97@gmail.com>
2025-09-26 18:30:52 +0000

    Fix: added favicons, started global translations

---
+++ lang/fr/globlal.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'site_name' => 'Laravel Sénégal',
+    'site_description' => 'Bienvenue sur le site de la plus grande communauté de développeurs Laravel au Sénégal.',
+    'navigation' => [
+        // TO DO
+    ],
+];
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,31 +1,35 @@
+@props(['title' => null, 'canonical' => null])
+
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full scroll-smooth">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ isset($title) ? $title . ' | ' : '' }} {{ __('global.site_name') }}</title>
 
-        <!-- Icons -->
-        <link rel="stylesheet" href=" {{ asset('images/Laravelsn.png') }}">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    {{-- Favivons --}}
+    @include('partials._favicons')
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Styles -->
-        @livewireStyles
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
-        </div>
+    <!-- Styles -->
+    @livewireStyles
+</head>
 
-        @livewireScripts
-        <x-toaster-hub />
-    </body>
+<body>
+    <div class="font-sans text-gray-900 antialiased">
+        {{ $slot }}
+    </div>
+
+    @livewireScripts
+    <x-toaster-hub />
+</body>
+
 </html>
