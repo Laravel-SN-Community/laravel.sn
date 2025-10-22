71ea80c934a5d257b428b25fc35360fbdfefacf3
Mountaga Diao Leye DIOP <73469916+h4r0n1@users.noreply.github.com>
2025-10-22 01:44:13 +0000

    Update title tag to include dynamic content

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>
+        {{ isset($title) && $title ? "$title | " : '' }}{{ config('app.name') }}
+        {{ request()->routeIs('welcome') ? ' - ' . 'La plus grande communauté de développeurs Laravel au Sénégal' : '' }}
+        </title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
