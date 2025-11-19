a370f691d2cf99a1a9e7160cc1981d85e69223c4
Mountaga Diao Leye Diop <mdl.diop97@gmail.com>
2025-11-19 14:48:57 +0000

    Update font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,48 +1,52 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/filament.css', 'resources/js/app.js'])
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
-        <!-- Styles -->
-        @filamentStyles
-        @livewireStyles
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/css/filament.css', 'resources/js/app.js'])
 
-        <!-- Mixpanel -->
-        @include('genealabs-laravel-mixpanel::partials.mixpanel')
-    </head>
-    <body class="h-full bg-gray-50">
-        
-        {{-- Top Navigation Bar --}}
-        <x-topbar/>
-        <div x-data="{ userDropdownOpen: false }" class="">
-           
-            {{-- Page Content --}}
-            <main class="py-6">
-                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    {{ $slot }}
-                </div>
-            </main>
-            
-            {{-- Future: Notification Components --}}
-            @livewire('notifications') 
-        </div>
-        
-        @stack('modals')
-        
-        @filamentScripts()
-        @livewireScripts
-        @vite(['resources/js/app.js'])
-    </body>
+    <!-- Styles -->
+    @filamentStyles
+    @livewireStyles
+
+    <!-- Mixpanel -->
+    @include('genealabs-laravel-mixpanel::partials.mixpanel')
+</head>
+
+<body class="h-full bg-gray-50">
+
+    {{-- Top Navigation Bar --}}
+    <x-topbar />
+    <div x-data="{ userDropdownOpen: false }" class="">
+
+        {{-- Page Content --}}
+        <main class="py-6">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                {{ $slot }}
+            </div>
+        </main>
+
+        {{-- Future: Notification Components --}}
+        @livewire('notifications')
+    </div>
+
+    @stack('modals')
+
+    @filamentScripts()
+    @livewireScripts
+    @vite(['resources/js/app.js'])
+</body>
+
 </html>
